e5180921dfccb6903adf393b9149e639062fe5b1
SagarPal98 <earningpoint973@gmail.com>
2025-06-01 13:40:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -98,7 +98,7 @@ class Bot(Client):
         await web.TCPSite(app, "0.0.0.0", PORT).start()
 
 
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ </blockquote></b>")
+        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ </blockquote></b>\n\nᴄʟɪᴄᴋ -- /start")
         except: pass
 
     async def stop(self, *args):
