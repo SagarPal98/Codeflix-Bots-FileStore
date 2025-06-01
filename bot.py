a867cab5e18e234182b91c05a016723a4cf5fab8
SagarPal98 <earningpoint973@gmail.com>
2025-06-01 11:10:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -81,20 +81,16 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/weebs_support for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Dark_AdminBot for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!")
-        self.LOGGER(__name__).info(f"""       
-
-Bot Started 😘 
- 
-                                          """)
+        self.LOGGER(__name__).info(f""" Bot Started 😘  """)
 
         self.set_parse_mode(ParseMode.HTML)
         self.username = usr_bot_me.username
-        self.LOGGER(__name__).info(f"Bot Running..! Made by @Codeflix_Bots")   
+        self.LOGGER(__name__).info(f"Bot Running..! 🤖")   
 
         # Start Web Server
         app = web.AppRunner(await web_server())
@@ -107,13 +103,13 @@ Bot Started 😘
 
     async def stop(self, *args):
         await super().stop()
-        self.LOGGER(__name__).info("Bot stopped.")
+        self.LOGGER(__name__).info("Bot stopped.🫡")
 
     def run(self):
         """Run the bot."""
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.start())
-        self.LOGGER(__name__).info("Bot is now running. Thanks to @rohit_1888")
+        self.LOGGER(__name__).info("Bot is now running. 👑")
         try:
             loop.run_forever()
         except KeyboardInterrupt:
